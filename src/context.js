a9a083d32b71542b02793a33df6a26d5b32815d3
Edan Schwartz <edanschwartz@gmail.com>
2022-01-27 13:22:49 -0600

    Add logging

--- src/context.js
+++ src/context.js
@@ -1,5 +1,5 @@
-import { createContext, useContext, useState } from "react";
-
+import { createContext, useContext, useState, useRef, useEffect } from "react";
+import * as Diff from 'diff';
 
 export const GlobalContext = createContext();
 
@@ -11,12 +11,23 @@ export function GlobalProvider({ children }) {
         'Bananas'
     ]);
 
+    const state = {
+        name, setName,
+        shoppingList, setShoppingList
+    }
+
+    // For logging only
+    const prevState = usePrevious(state);
+
+    useEffect(() => {
+        // Log previous and next state
+        console.log('%cprev state', 'color: grey; font-weight: bold', objWithoutFns(prevState));
+        console.log('%cnext state', 'color: green; font-weight: bold', objWithoutFns(state));
+    });
+
     return (
         <GlobalContext.Provider
-            value={{
-                name, setName,
-                shoppingList, setShoppingList
-            }}
+            value={state}
         >
             {children}
         </GlobalContext.Provider>
@@ -26,4 +37,30 @@ export function GlobalProvider({ children }) {
 // If you want a shorthand..
 export function useGlobalContext() {
     return useContext(GlobalContext);
+}
+
+// For logging only
+// https://blog.logrocket.com/how-to-get-previous-props-state-with-react-hooks/
+function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+        ref.current = value;
+    });
+    return ref.current;
+}
+
+function objWithoutFns(obj) {
+    let newObj = {};
+
+    if (!obj) {
+        return;
+    }
+
+    for (let [key, val] of Object.entries(obj)) {
+        if (typeof val !== 'function') {
+            newObj[key] = val;
+        }
+    }
+
+    return newObj;
 }
